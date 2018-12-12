f26efe316d1e60f28f933832a2b9562fb2b9b225
jakebostic <jake_bostic@yahoo.com>
2018-12-12 12:20:01 -0500

    created while loop

--- RectangleCalculator/Program.cs
+++ RectangleCalculator/Program.cs
@@ -9,20 +9,25 @@ namespace RectangleCalculator
             Console.WriteLine("Welcome to the Area and Perimeter Calculator");
             Console.WriteLine();
 
-            Console.Write("Enter length: ");
-            if (double.TryParse(Console.ReadLine(), out double length))
+            string choice = "y";
+            while (choice.Equals("y"))
             {
-                Console.Write("Enter width: ");
-                if (double.TryParse(Console.ReadLine(), out double width))
+                Console.Write("Enter length: ");
+                if (double.TryParse(Console.ReadLine(), out double length))
                 {
-                    double area = width * length;
-                    double per = 2 * width + 2 * length;
-                    Console.WriteLine($"\nArea: {area}");
-                    Console.WriteLine($"\nPerimeter: {per}");
-                }
+                    Console.Write("Enter width: ");
+                    if (double.TryParse(Console.ReadLine(), out double width))
+                    {
+                        double area = width * length;
+                        double per = 2 * width + 2 * length;
+                        Console.WriteLine($"\nArea: {area}");
+                        Console.WriteLine($"\nPerimeter: {per}");
+                        Console.WriteLine();
+                    }
 
-                Console.Write("Press any key to exit...");
-                Console.ReadKey(true);
+                    Console.Write("Continue? (y/n)");
+                    choice = Console.ReadLine();
+                }
             }
             
         
