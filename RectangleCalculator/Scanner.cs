18de66746838aa08e5b2b5f82df7659a8e5b0159
jakebostic <jake_bostic@yahoo.com>
2018-12-14 14:55:53 -0500

    created scanner class

---
+++ RectangleCalculator/Scanner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleCalculator
+{
+    class Scanner : System.IO.StringReader
+    {
+        string currentWord;
+
+        public Scanner(string source) : base(source)
+        {
+            readNextWord();
+        }
+
+        private void readNextWord()
+        {
+            System.Text.StringBuilder sb = new StringBuilder();
+            char nextChar;
+            int next;
+            do
+            {
+                next = this.Read();
+                if (next < 0)
+                    break;
+                nextChar = (char)next;
+                if (char.IsWhiteSpace(nextChar))
+                    break;
+                sb.Append(nextChar);
+            } while (true);
+            while ((this.Peek() >= 0) && (char.IsWhiteSpace((char)this.Peek())))
+                this.Read();
+            if (sb.Length > 0)
+                currentWord = sb.ToString();
+            else
+                currentWord = null;
+        }
+
+        public bool hasNextDouble()
+        {
+            if (currentWord == null)
+                return false;
+            double dummy;
+            return double.TryParse(currentWord, out dummy);
+        }
+
+        public double nextDouble()
+        {
+            try
+            {
+                return double.Parse(currentWord);
+            }
+            finally
+            {
+                readNextWord();
+            }
+        }
+
+
+    }
+}
